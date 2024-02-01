38b93f98c73bd29f656d847fb4202557d9617d05
BriceBZH <brice.rubeaux@wanadoo.fr>
2024-02-01 16:01:30 +0100

    modifs autoload Brice

--- config/autoload.php
+++ config/autoload.php
@@ -1,13 +1,12 @@
 <?php
     require "models/Category.php";
     require "models/Post.php";
-    require "models/Salon.php";
+    require "models/Channel.php";
     require "models/User.php";
     require "managers/AbstractManager.php";
     require "managers/CategoryManager.php";
     require "managers/PostManager.php";
-    require "managers/SalonManager.php";
+    require "managers/ChannelManager.php";
     require "managers/UserManager.php";
     require "controllers/PageController.php";
-    require "controllers/AuthController.php";
     require "config/Router.php";
