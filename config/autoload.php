ccbf3cb5464019e9984a4c7de87348d7a612fdb7
Wanderlust-29 <ryanroudaut8@gmail.com>
2024-02-02 09:32:20 +0000

    modi autoload

--- config/autoload.php
+++ config/autoload.php
@@ -1,23 +1,12 @@
 <?php
     require "models/Category.php";
     require "models/Post.php";
-<<<<<<< HEAD
-    // require "models/Salon.php";
-=======
     require "models/Channel.php";
->>>>>>> 6b21aa7f5ca82f34e51073ca99ab99d8fa2947af
     require "models/User.php";
     require "managers/AbstractManager.php";
     require "managers/CategoryManager.php";
     require "managers/PostManager.php";
-<<<<<<< HEAD
-    // require "managers/SalonManager.php";
-    require "managers/UserManager.php";
-    require "controllers/PageController.php";
-    // require "controllers/AuthController.php";
-=======
     require "managers/ChannelManager.php";
     require "managers/UserManager.php";
     require "controllers/PageController.php";
->>>>>>> 6b21aa7f5ca82f34e51073ca99ab99d8fa2947af
     require "config/Router.php";
